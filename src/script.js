05fce2960a8f806d7c0ef848156ec451ec5dd7a8
Cristina F <77860767+cristinafor@users.noreply.github.com>
2021-02-02 22:47:45 +0100

    Added Api call to Openweather

--- src/script.js
+++ src/script.js
@@ -1,22 +1,47 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`
+    }
+    let days = ["Sunday", 
+                "Monday", 
+                "Tuesday", 
+                "Wednesday", 
+                "Thursday", 
+                "Friday", 
+                "Saturday"];
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+}
+
+
 function displayTemperature(response) {
     console.log(response.data);
     let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = Math.round(response.data.main.temp);
     let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = response.data.name;
     let descriptionElement = document.querySelector("#description");
-    descriptionElement.innerHTML = response.data.weather[0].description;
     let humidityElement = document.querySelector("#humidity");
-    humidityElement.innerHTML = response.data.main.humidity;
     let windElement = document.querySelector("#wind-speed");
-    windElement.innerHTML = Math.round(response.data.wind.speed);
     let feelElement = document.querySelector("#feel");
+    let dateTime = document.querySelector("#date-time");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
     feelElement.innerHTML = Math.round(response.data.main.feels_like);
+    dateTime.innerHTML = formatDate(response.data.dt * 1000);
 
 }
 
 let apiKey = "aef208e4266d921b0a65e7200cab5d80";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Berlin&appid=${apiKey}&units=metric`;
+let city = "Berlin"
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 let units = "metric";
 
 axios.get(apiUrl).then(displayTemperature);
