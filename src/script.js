7a2d1c51c89639438024c6b08358698b98904652
Cristina F <77860767+cristinafor@users.noreply.github.com>
2021-02-12 20:50:25 +0100

    Updated forecast for current position

--- src/script.js
+++ src/script.js
@@ -89,6 +89,9 @@ function displayPosition(position) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function getCurrentPosition(event) {
